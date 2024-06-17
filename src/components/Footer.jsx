69947a993187bb1960a5df3014af7106f0e5238d
Mahsa <50694999+Mah-tab88@users.noreply.github.com>
2024-06-17 14:34:14 -0700

    revise

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,262 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import { FaFacebook } from "react-icons/fa6";
+import { TiSocialInstagram } from "react-icons/ti";
+import { FaLinkedin } from "react-icons/fa";
+import { FaSquareTwitter } from "react-icons/fa6";
+import { FaSquareYoutube } from "react-icons/fa6";
+import { TfiWorld } from "react-icons/tfi";
 
 export default function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <div className="bg-gray-100 py-11">
+      <div className="w-4/5 m-auto">
+        <div className="grid grid-cols-5 mb-9">
+          <div className="">
+            <h5 className="font-semibold text-gray-600">Categories</h5>
+            <ul>
+              <li>
+                <Link className="text-gray-400 text-sm">
+                  Graphics & Designs
+                </Link>
+              </li>
+              <li>
+                <Link className="text-gray-400 text-sm">Degital Marketing</Link>
+              </li>
+              <li>
+                <Link className="text-gray-400 text-sm">
+                  Writting & Translation
+                </Link>
+              </li>
+              <li>
+                <Link className="text-gray-400 text-sm">
+                  Videos & Animation
+                </Link>
+              </li>
+              <li>
+                <Link className="text-gray-400 text-sm">Music & Audio</Link>
+              </li>
+              <li>
+                <Link className="text-gray-400 text-sm">
+                  Programming & Tech
+                </Link>
+              </li>
+              <li>
+                <Link className="text-gray-400 text-sm">Data</Link>
+              </li>
+              <li>
+                <Link className="text-gray-400 text-sm">Business</Link>
+              </li>
+              <li>
+                <Link className="text-gray-400 text-sm">Photography</Link>
+              </li>
+              <li>
+                <Link className="text-gray-400 text-sm">SiteMap</Link>
+              </li>
+            </ul>
+          </div>
+
+          <div>
+            <h5 className="font-semibold text-gray-600">Categories</h5>
+            <ul>
+              <li>
+                <Link className="text-gray-400 text-sm">
+                  Graphics & Designs
+                </Link>
+              </li>
+              <li>
+                <Link className="text-gray-400 text-sm">Degital Marketing</Link>
+              </li>
+              <li>
+                <Link className="text-gray-400 text-sm">
+                  Writting & Translation
+                </Link>
+              </li>
+              <li>
+                <Link className="text-gray-400 text-sm">
+                  Videos & Animation
+                </Link>
+              </li>
+              <li>
+                <Link className="text-gray-400 text-sm">Music & Audio</Link>
+              </li>
+              <li>
+                <Link className="text-gray-400 text-sm">
+                  Programming & Tech
+                </Link>
+              </li>
+              <li>
+                <Link className="text-gray-400 text-sm">Data</Link>
+              </li>
+              <li>
+                <Link className="text-gray-400 text-sm">Business</Link>
+              </li>
+              <li>
+                <Link className="text-gray-400 text-sm">Photography</Link>
+              </li>
+              <li>
+                <Link className="text-gray-400 text-sm">SiteMap</Link>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h5 className="font-semibold text-gray-600">Categories</h5>
+            <ul>
+              <li>
+                <Link className="text-gray-400 text-sm">
+                  Graphics & Designs
+                </Link>
+              </li>
+              <li>
+                <Link className="text-gray-400 text-sm">Degital Marketing</Link>
+              </li>
+              <li>
+                <Link className="text-gray-400 text-sm">
+                  Writting & Translation
+                </Link>
+              </li>
+              <li>
+                <Link className="text-gray-400 text-sm">
+                  Videos & Animation
+                </Link>
+              </li>
+              <li>
+                <Link className="text-gray-400 text-sm">Music & Audio</Link>
+              </li>
+              <li>
+                <Link className="text-gray-400 text-sm">
+                  Programming & Tech
+                </Link>
+              </li>
+              <li>
+                <Link className="text-gray-400 text-sm">Data</Link>
+              </li>
+              <li>
+                <Link className="text-gray-400 text-sm">Business</Link>
+              </li>
+              <li>
+                <Link className="text-gray-400 text-sm">Photography</Link>
+              </li>
+              <li>
+                <Link className="text-gray-400 text-sm">SiteMap</Link>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h5 className="font-semibold text-gray-600">Categories</h5>
+            <ul>
+              <li>
+                <Link className="text-gray-400 text-sm">
+                  Graphics & Designs
+                </Link>
+              </li>
+              <li>
+                <Link className="text-gray-400 text-sm">Degital Marketing</Link>
+              </li>
+              <li>
+                <Link className="text-gray-400 text-sm">
+                  Writting & Translation
+                </Link>
+              </li>
+              <li>
+                <Link className="text-gray-400 text-sm">
+                  Videos & Animation
+                </Link>
+              </li>
+              <li>
+                <Link className="text-gray-400 text-sm">Music & Audio</Link>
+              </li>
+              <li>
+                <Link className="text-gray-400 text-sm">
+                  Programming & Tech
+                </Link>
+              </li>
+              <li>
+                <Link className="text-gray-400 text-sm">Data</Link>
+              </li>
+              <li>
+                <Link className="text-gray-400 text-sm">Business</Link>
+              </li>
+              <li>
+                <Link className="text-gray-400 text-sm">Photography</Link>
+              </li>
+              <li>
+                <Link className="text-gray-400 text-sm">SiteMap</Link>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h5 className="font-semibold text-gray-600">Categories</h5>
+            <ul>
+              <li>
+                <Link className="text-gray-400 text-sm">
+                  Graphics & Designs
+                </Link>
+              </li>
+              <li>
+                <Link className="text-gray-400 text-sm">Degital Marketing</Link>
+              </li>
+              <li>
+                <Link className="text-gray-400 text-sm">
+                  Writting & Translation
+                </Link>
+              </li>
+              <li>
+                <Link className="text-gray-400 text-sm">
+                  Videos & Animation
+                </Link>
+              </li>
+              <li>
+                <Link className="text-gray-400 text-sm">Music & Audio</Link>
+              </li>
+              <li>
+                <Link className="text-gray-400 text-sm">
+                  Programming & Tech
+                </Link>
+              </li>
+              <li>
+                <Link className="text-gray-400 text-sm">Data</Link>
+              </li>
+              <li>
+                <Link className="text-gray-400 text-sm">Business</Link>
+              </li>
+              <li>
+                <Link className="text-gray-400 text-sm">Photography</Link>
+              </li>
+              <li>
+                <Link className="text-gray-400 text-sm">SiteMap</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <hr  />
+        <div className="flex justify-between items-center mt-3">
+          <h4 className="text-gray-500 font-extrabold">FIVERR.</h4>
+          <div className="flex justify-between items-center gap-4">
+            <Link className="text-gray-400 text-xl">
+              <FaFacebook />
+            </Link>
+            <Link className="text-gray-400 text-xl">
+              <TiSocialInstagram />
+            </Link>
+            <Link className="text-gray-400 text-xl">
+              <FaLinkedin />
+            </Link>
+            <Link className="text-gray-400 text-xl">
+              <FaSquareTwitter />
+            </Link>
+            <Link className="text-gray-400 text-xl">
+              <FaSquareYoutube />
+            </Link>
+
+            <div className="flex justify-center items-center gap-2 text-gray-400 ">
+              <TfiWorld className="text-xl"/>
+
+              <span>English</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,8 @@ import Slide from "../components/Slide";
 import Slider from "react-slick";
 import { CiCircleCheck } from "react-icons/ci";
 export default function Home() {
-  const sliderGig = useRef(null);
+  const sliderCart = useRef(null);
+  const sliderProject = useRef(null);
   // const [carts, setCarts] = useState([]);
   const carts = [
     {
@@ -56,7 +57,7 @@ export default function Home() {
       img: "https://images.pexels.com/photos/15032623/pexels-photo-15032623.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
     },
   ];
-  const settingGig = {
+  const settingCart = {
     dots: false,
     infinite: true,
     arrows: false,
@@ -65,8 +66,77 @@ export default function Home() {
     slidesToScroll: 5,
     arrows: true,
   };
+
+  const projects = [
+    {
+      id: 1,
+      img: "https://images.pexels.com/photos/1462935/pexels-photo-1462935.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      pp: "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      cat: "Web and Mobile Design",
+      username: "Anna Bell",
+    },
+    {
+      id: 2,
+      img: "https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      pp: "https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      cat: "Logo Design",
+      username: "Morton Green",
+    },
+    {
+      id: 3,
+      img: "https://images.pexels.com/photos/4144923/pexels-photo-4144923.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      pp: "https://images.pexels.com/photos/1181686/pexels-photo-1181686.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      cat: "Animated GIFs",
+      username: "Emmett Potter",
+    },
+    {
+      id: 4,
+      img: "https://images.pexels.com/photos/4348404/pexels-photo-4348404.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      pp: "https://images.pexels.com/photos/2182970/pexels-photo-2182970.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      cat: "Packaging Design",
+      username: "Freddie Johnston",
+    },
+    {
+      id: 5,
+      img: "https://images.pexels.com/photos/4458554/pexels-photo-4458554.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      pp: "https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      cat: "Social Media Design",
+      username: "Audrey Richards",
+    },
+    {
+      id: 6,
+      img: "https://images.pexels.com/photos/4465831/pexels-photo-4465831.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      pp: "https://images.pexels.com/photos/1036627/pexels-photo-1036627.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      cat: "Illustration",
+      username: "Dalton Hudson",
+    },
+    {
+      id: 7,
+      img: "https://images.pexels.com/photos/6077368/pexels-photo-6077368.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      pp: "https://images.pexels.com/photos/1858175/pexels-photo-1858175.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      cat: "Book Design",
+      username: "Hannah Dougherty",
+    },
+    {
+      id: 8,
+      img: "https://images.pexels.com/photos/4065876/pexels-photo-4065876.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      pp: "https://images.pexels.com/photos/1680175/pexels-photo-1680175.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      cat: "Digital Marketing",
+      username: "Ward Brewer",
+    },
+  ];
+  const settingProject = {
+    dots: false,
+    infinite: true,
+    arrows: false,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    arrows: true,
+  };
   return (
     <div>
+      {/*  poster */}
       <div className="bg-websiteColor headrt ">
         <div className="max-w-5xl m-auto flex justify-between items-center">
           <div className="text-white">
@@ -77,9 +147,9 @@ export default function Home() {
             <div className="flex justify-between items-center mt-8 border border-transparent overflow-hidden  rounded-md ">
               <div className="flex justify-between items-center w-full bg-white">
                 <IoSearchOutline className="text-slate-900 mx-2 font-semibold " />
-                <input className="py-2 w-full border-none outline-none text-slate-700 font-semibold text-lg" />
+                <input className="py-2 w-full border-none outline-none text-slate-700 font-semibold text-lg h-11" />
               </div>
-              <button className="px-3 bg-green-500 py-2 hover:bg-green-700">
+              <button className="px-3 bg-green-500 py-2 h-11 hover:bg-green-700">
                 Search
               </button>
             </div>
@@ -106,6 +176,8 @@ export default function Home() {
           </div>
         </div>
       </div>
+
+      {/*  trust by */}
       <div className="bg-slate-100 flex justify-center items-center gap-6 py-6">
         <p className="text-gray-400 font-semibold">Trusted By: </p>
         <div className="flex justify-center items-center gap-4 ">
@@ -116,8 +188,10 @@ export default function Home() {
           <span className="text-gray-400">PayPal</span>
         </div>
       </div>
+
+      {/*  slider carts */}
       <div className=" m-auto my-16 bg-green-300">
-        <Slider ref={sliderGig} {...settingGig} className="max-w-4xl m-auto">
+        <Slider ref={sliderCart} {...settingCart} className="max-w-4xl m-auto">
           {carts.map((c) => {
             return (
               <Slide>
@@ -136,16 +210,18 @@ export default function Home() {
           })}
         </Slider>
       </div>
+
+      {/*  fingertips */}
       <div className=" bg-green-100 py-9">
         <div className="w-5/6 m-auto flex justify-between items-center gap-5">
           <div className="mb-4">
             <h3 className="font-bold text-xl mb-6">
               A whole world of freelance talent at
-              <br /> your fingettips
+              <br /> your fingertips
             </h3>
             <div className="mb-3">
               <div className="flex justify-start items-center">
-                <CiCircleCheck className="mr-2"/>
+                <CiCircleCheck className="mr-2" />
                 <h5 className="text-slate-700 font-semibold text-lg">
                   The best for every budget
                 </h5>
@@ -157,7 +233,7 @@ export default function Home() {
             </div>
             <div className="mb-3">
               <div className="flex justify-start items-center">
-                <CiCircleCheck className="mr-2"/>
+                <CiCircleCheck className="mr-2" />
                 <h5 className="text-slate-700 font-semibold text-lg">
                   Quality work done quickly
                 </h5>
@@ -168,7 +244,7 @@ export default function Home() {
             </div>
             <div className="mb-3">
               <div className="flex justify-start items-center">
-                <CiCircleCheck className="mr-2"/>
+                <CiCircleCheck className="mr-2" />
                 <h5 className="text-slate-700 font-semibold text-lg">
                   Protected payments, every time
                 </h5>
@@ -180,7 +256,7 @@ export default function Home() {
             </div>
             <div className="mb-3">
               <div className="flex justify-start items-center">
-                <CiCircleCheck className="mr-2"/>
+                <CiCircleCheck className="mr-2" />
                 <h5 className="text-slate-700 font-semibold text-lg">
                   Protected payments, every time
                 </h5>
@@ -196,6 +272,100 @@ export default function Home() {
             <img src="././public/camp.jpg" className="rounded-md" />
           </div>
         </div>
+      </div>
+
+      {/* tech */}
+      <div className="bg-blue-950  my-20 py-8">
+        <div className="w-5/6 m-auto flex justify-between items-center ">
+          <div className=" w-2/5">
+            <div className="flex justify-start items-end">
+              <h2 className="text-white font-bold text-3xl">Fivver</h2>
+              <i className=" text-white ml-3 text-3xl">Business</i>
+            </div>
+            <h4 className="text-white my-6 text-2xl font-semibold">
+              A business solution for design <br />{" "}
+              <i className="font-normal">teams</i>
+            </h4>
+            <h3 className="text-slate-300">
+              Update to a curated experience packed with tools and benefite to
+              dedicated business
+            </h3>
+            <div className="my-6">
+              <div className="flex justify-start items-center mb-3">
+                <CiCircleCheck className="mr-2 text-slate-100" />
+                <h5 className="text-slate-100  text-lg">
+                  Protected payments, every time
+                </h5>
+              </div>
+              <div className="flex justify-start items-center mb-3">
+                <CiCircleCheck className="mr-2 text-slate-100" />
+                <h5 className="text-slate-100  text-lg">
+                  Quality work done quickly
+                </h5>
+              </div>
+              <div className="flex justify-start items-center mb-3">
+                <CiCircleCheck className="mr-2 text-slate-100" />
+                <h5 className="text-slate-100  text-lg">
+                  The best for every budget
+                </h5>
+              </div>
+              <button className="bg-emerald-600 px-2 py-1 rounded-md text-white my-2 hover:bg-emerald-800">
+                Explore Feverr Business
+              </button>
+            </div>
+          </div>
+          <div className=" w-2/5 ">
+            <div className="relative">
+              <img src="././public/ax.png" />
+              <div className="absolute bottom-4 right-24">
+                <p className="text-xs bg-violet-200 w-36  p-1  rounded-md ">
+                  Lorem ipsum dolor sit amet. Aut .
+                </p>
+              </div>
+              <div className="absolute top-4 -left-24">
+                <p className="text-xs bg-orange-200 w-36  p-1  rounded-md ">
+                  Lorem ipsum dolor sit amet. Aut .
+                </p>
+              </div>
+              <div className="absolute bottom-1/3 -left-32">
+                <p className="text-xs bg-stone-200 w-36  p-1  rounded-md ">
+                  Lorem ipsum dolor sit amet. Aut .
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* slider products */}
+      <div className=" m-auto my-16 bg-green-300">
+        <Slider
+          ref={sliderProject}
+          {...settingProject}
+          className="max-w-4xl m-auto"
+        >
+          {projects.map((c) => {
+            return (
+              <Slide>
+                <div
+                  key={c.id}
+                  className=" mx-3 cursor-pointer rounded-md  overflow-hidden "
+                >
+                  <img src={c.img} className=" h-44 " />
+                  <div className=" flex justify-between items-center h-24  text-sm bg-white px-2 pt-2 font-semibold">
+                    <div className="w-1/2 ">
+                      <img src={c.pp} className="w-10 h-10 rounded-full" />
+                    </div>
+                    <div>
+                      <p className="text-xs text-gray-400">{c.cat}</p>
+                      <p className="text-xs text-gray-500">{c.username}</p>
+                    </div>
+                  </div>
+                </div>
+              </Slide>
+            );
+          })}
+        </Slider>
       </div>
     </div>
   );
